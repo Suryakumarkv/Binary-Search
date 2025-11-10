66fda937bb1c36e292366a212055bca04ecefbbc
Surya Kumar K V <suryakumarkv30@gmail.com>
2025-11-10 12:04:03 +0530

    First Commit

---
+++ Problems/src/FloorOfAnArrayNumber.java
@@ -0,0 +1,35 @@
+//The floor of a number x in a sorted array is:
+//the largest element in the array less than or equal to x.
+
+public class FloorOfAnArrayNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int x = 9;
+        int Floor = FloorOfanElem(arr, x);
+        System.out.println("Floor of " + x + " is: " + Floor);
+    }
+
+    private static int FloorOfanElem(int[] arr, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] == x) {
+                return arr[mid];
+            } else if(arr[mid] > x) {
+                ans = arr[mid];
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return arr[right];
+    }
+}
+
+//Ceiling of a number -> Smallest Element in array that is greater or equal to target element
+//if element is present then retuen it or return start instead -1 from binary Search
+
