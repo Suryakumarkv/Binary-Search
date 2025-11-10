99fbc0de18d170f370df94da3a3b01bcbd09246c
Surya Kumar K V <suryakumarkv30@gmail.com>
2025-11-10 11:14:19 +0530

    Initial commit

---
+++ Problems/src/CeilingOfANumber.java
@@ -0,0 +1,7 @@
+//Ceiling of a number -> Smallest Element in array that is greater or equal to target element
+
+public class CeilingOfANumber {
+    public static void main(String[] args) {
+
+    }
+}
